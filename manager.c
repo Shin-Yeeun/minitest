dac4cc894c08daf541d4c45c436717203326f398
Shin Yeeun <21900400@handong.edu>
2020-04-26 03:21:08 +0900

    Update saveData

--- manager.c
+++ manager.c
@@ -37,10 +37,14 @@ int selectDataNo(Product *p, int count){
 //배열데이터를 파일에 저장하는 함수
 void saveData(Product p[], int count){
 	FILE* fp;
-
 	//중량 가격 제품명
 	fp= fopen("product.txt","wt");
 	
+	for(int i=0 ;i<count; i++){
+		if(p[i].price!=-1){
+			fprintf(fp,"%d %d %s",p[i].weight, p[i].price, p[i].name);
+}
+}
 	
 	fclose(fp);
 	printf("저장됨!\n");
